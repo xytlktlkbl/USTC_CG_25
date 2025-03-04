ffc542ca7a230505250bd87eb3971d650bcedbe9
wuwzh <wuwzh001@gmail.com>
2025-03-04 15:17:45 +0800

    update rbf

---
+++ Framework2D/src/assignments/2_ImageWarping/warper/RBF_warper.h
@@ -0,0 +1,17 @@
+// HW2_TODO: Implement the RBFWarper class
+#pragma once
+
+#include "warper.h"
+
+namespace USTC_CG
+{
+class RBFWarper : public Warper
+{
+   public:
+    RBFWarper() = default;
+    virtual ~RBFWarper() = default;
+    // HW2_TODO: Implement the warp(...) function with RBF interpolation
+
+    // HW2_TODO: other functions or variables if you need
+};
+}  // namespace USTC_CG
